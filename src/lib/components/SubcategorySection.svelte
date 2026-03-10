2ea0cab8cc50dcb906c080254d677f64ae260007
Wietse de Vries <wietsedv@proton.me>
2026-03-10 15:38:15 +0100

    fix container responsiveness

--- src/lib/components/SubcategorySection.svelte
+++ src/lib/components/SubcategorySection.svelte
@@ -12,17 +12,19 @@
 	];
 </script>
 
-<div class="text-2xl font-semibold text-white">{title}</div>
-<div class="text-white">{description}</div>
+<div class="px-4">
+	<div class="text-2xl font-semibold text-white">{title}</div>
+	<div class="text-white">{description}</div>
 
-<div class="my-6 grid grid-cols-4 gap-3">
-	{#each levels as level (level.id)}
-		<a
-			href={resolve('/')}
-			class="rounded-lg bg-white px-2 py-6 text-center transition hover:bg-gray-100 hover:shadow-xl"
-		>
-			<div class="mb-4 text-xl font-medium">Level {level.id}</div>
-			<Score score={level.score} />
-		</a>
-	{/each}
+	<div class="xs:grid-cols-3 2xs:grid-cols-2 my-6 grid gap-3 sm:grid-cols-4">
+		{#each levels as level (level.id)}
+			<a
+				href={resolve('/')}
+				class="rounded-lg bg-white px-2 py-6 text-center transition hover:bg-gray-100 hover:shadow-xl"
+			>
+				<div class="mb-4 text-xl font-medium">Level {level.id}</div>
+				<Score score={level.score} />
+			</a>
+		{/each}
+	</div>
 </div>
